d060fb902a056a75d53ebf6cb5bf1021aeb3087e
Albert Min <>
2018-01-17 13:25:18 -0500

    client side render

--- src/client/client.js
+++ src/client/client.js
@@ -1,3 +1,7 @@
 // startup point for client side app
 
-console.log('Weeeee!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home';
+
+ReactDOM.hydrate(<Home />, document.querySelector('#root'));
--- src/client/components/home.js
+++ src/client/components/home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>I'm the most bestest home component</div>
+  return (
+    <div>
+      <div>I'm the most bestest home component</div>
+      <button onClick={() => console.log('Yoooooo')}>Click me bro</button>
+    </div>
+  );
 };
 
 export default Home;
