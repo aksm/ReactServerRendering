aa44ee33dd871cc8b22f5f5228299b12e3e28b9d
Albert Min <>
2018-01-23 21:02:28 -0500

    client rendering of users

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
+import 'babel-polyfill';
 import express from 'express';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 
 const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
+  const store = createStore();
 
-  res.send(renderer(req));
+  // logic to initialize and load data into the store
+
+  res.send(renderer(req, store));
 });
 
 app.listen(3000, () => {
